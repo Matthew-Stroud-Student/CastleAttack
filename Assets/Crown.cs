e352cbef2497c6ee80bb6bfe414651cf07d9ce1d
Matthew Stroud <mast224@student.oaisd.org>
2025-03-18 08:49:53 -0400

    Added Game.

---
+++ Assets/Crown.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crown : MonoBehaviour
+{
+    public GameObject explosion;
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Score score = FindObjectOfType<Score>();
+            if (score)
+            {
+                score.EndLevel();
+            }
+
+            if(explosion)
+            {
+                Instantiate(explosion, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
